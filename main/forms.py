90cb83220a8b5b9777ff9b33b16e225740c77987
Steve Roach <steve_roach@hotmail.com>
2023-05-07 21:44:02 +1000

    a001

--- main/forms.py
+++ main/forms.py
@@ -2,7 +2,14 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=255)
-    subject = forms.CharField(max_length=255)
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea)
+    name = forms.CharField(label=False, max_length=255, widget=forms.TextInput(
+        attrs={'placeholder': 'Name', 'style': 'width: 300px;', 'class': 'form-control'}))
+    subject = forms.CharField(label=False, max_length=255, widget=forms.TextInput(
+        attrs={'placeholder': 'Subject', 'style': 'width: 300px;', 'class':
+            'form-control'}))
+    email = forms.EmailField(label=False, max_length=255, widget=forms.TextInput(
+        attrs={'placeholder': 'email Address', 'style': 'width: 300px;',
+               'class': 'form-control'}))
+    message = forms.CharField(label=False, widget=forms.Textarea(
+        attrs={'placeholder': 'Message', 'style': 'width: 300px;', 'class':
+            'form-control'}))
