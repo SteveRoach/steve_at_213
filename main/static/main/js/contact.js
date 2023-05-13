c04891967f25e57ac2bed220d63e2edf6bcf8e73
Steve Roach <steve_roach@hotmail.com>
2023-05-14 07:47:36 +1000

    email ready 6

--- main/static/main/js/contact.js
+++ main/static/main/js/contact.js
@@ -1,10 +1,16 @@
-
 $(document).ready(function(){
-    contact_form_submitted = localStorage.getItem("contactFormSubmitted");
-    console.log(contact_form_submitted);
+    console.log("ready - START");
 
-    if(contact_form_submitted == 'true'){
-       console.log('submitted');
+    var form_errors = $('.errorlist').length;
+
+    console.log("ready - form_errors: " + form_errors);
+
+    contact_form_submitted = localStorage.getItem("contactFormSubmitted");
+
+    console.log("ready - contact_form_submitted: " + contact_form_submitted);
+
+    if(contact_form_submitted == 'true' && form_errors == 0){
+       console.log('ready - submitted && form_errors = 0: show sent');
 
        $("#sentModal").modal('show');
 
@@ -12,12 +18,10 @@ $(document).ready(function(){
         localStorage.setItem("contactFormSubmitted", contact_form_submitted);
     }
 
-    $("form").on('submit', function(){
+    $('form').on('submit', function(){
         contact_form_submitted = 'true';
         localStorage.setItem("contactFormSubmitted", contact_form_submitted);
-
-        $("#submit-button").removeClass('btn-primary');
-        $("#submit-button").addClass('btn-warning');
-        $("#submit-button").val('Sending...');
     });
+
+    console.log("ready - END");
 });
