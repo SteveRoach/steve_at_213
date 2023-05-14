0a7e7ed28862e7449ceaf28edad40157dc9613b9
Steve Roach <steve_roach@hotmail.com>
2023-05-14 14:54:10 +1000

    email ready 7

--- main/static/main/js/contact.js
+++ main/static/main/js/contact.js
@@ -1,27 +1,49 @@
 $(document).ready(function(){
-    console.log("ready - START");
+    console.log = function() {} // Comment this line to turn on logging
+
+    console.group('contact.js - ready');
 
     var form_errors = $('.errorlist').length;
 
-    console.log("ready - form_errors: " + form_errors);
+    console.log("form_errors: " + form_errors);
 
     contact_form_submitted = localStorage.getItem("contactFormSubmitted");
 
-    console.log("ready - contact_form_submitted: " + contact_form_submitted);
+    console.log("contact_form_submitted: " + contact_form_submitted);
 
     if(contact_form_submitted == 'true' && form_errors == 0){
-       console.log('ready - submitted && form_errors = 0: show sent');
+        console.group('submitted && form_errors = 0');
 
-       $("#sentModal").modal('show');
+        $("#sentModal").modal('show');
 
         contact_form_submitted = 'false';
         localStorage.setItem("contactFormSubmitted", contact_form_submitted);
+
+        console.groupEnd(); // Close the "submitted && form_errors = 0" group
     }
 
-    $('form').on('submit', function(){
+    $('form').submit(function(e)
+    {
+        console.group('on submit');
+
+        // Stop submit
+        e.preventDefault();
+
         contact_form_submitted = 'true';
         localStorage.setItem("contactFormSubmitted", contact_form_submitted);
+
+        $('#submit-button').css("color", "black");
+        $('#submit-button').css("background-color", "green");
+        $('#submit-button').val("Sending...");
+        $('#submit-button').prop('disabled', true);
+
+        console.groupEnd(); // Close the "on submit" group
+
+        alert('x');
+
+        // Submit
+        this.submit();
     });
 
-    console.log("ready - END");
+    console.groupEnd(); // Close the "contact.js - ready" group
 });
